b58da5c014c68e4150e8b43c67b40fdd4a382345
Emirhan Yener <yeneremirhann@gmail.com>
2022-05-20 23:09:05 +0300

    update test

--- tests/AddExportTest.js
+++ tests/AddExportTest.js
@@ -1,6 +1,6 @@
-list.add("interface", "IShape", "", "", [["public", "int", "calculate"], ["private", "string", "shapeName"]]);
-list.add("class", "Circle", "IShape", "interface", [["public", "int", "calculate"], ["private", "string", "shapeName"]]);
-list.add("class", "Rectangle", "IShape", "interface", [["public", "int", "calculate"], ["private", "string", "shapeName"]]);
-list.add("class", "Square", "Rectangle", "class", [["public", "int", "calculate"], ["private", "string", "shapeName"]]);
+list.add("interface", "IShape", "", "", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
+list.add("class", "Circle", "IShape", "interface", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
+list.add("class", "Rectangle", "IShape", "interface", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
+list.add("class", "Square", "Rectangle", "class", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
 refreshValues();
 exportClass();
