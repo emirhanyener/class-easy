ab4cff2acebd7c7c9ee99b1a208ce6244ed9e7ee
Emirhan Yener <yeneremirhann@gmail.com>
2022-05-20 19:29:48 +0300

    add other language method

--- js/export/CPlusPlusExporter.js
+++ js/export/CPlusPlusExporter.js
@@ -12,16 +12,61 @@ class CPlusPlusExporter extends ILanguageExporter{
 			values += "<div class = 'exportedClass'>"
 			values += "class " + iterator.get().className;
 			if(iterator.get().superClassName != ""){
-				values += " : " + iterator.get().superClassName;
+				values += " : public " + iterator.get().superClassName;
 			}
 			values += "{<br>";
 			//class scope
-			if(iterator.get().classType == "class")
-				values += tabSpace + "public:<br> " + tabSpace + iterator.get().className + "() {<br><br>" + tabSpace + "}";
-			
+			if(this.countPrivateMethods(iterator)){
+				values += tabSpace + "private:<br> ";
+				for(let i = 0; i < iterator.get().methods.length; i++){
+					if(iterator.get().methods[i][0] == "private"){
+						if(iterator.get().classType == "class"){
+							if(iterator.get().methods[i][1] == "void")
+								values += tabSpace + "void "+ iterator.get().methods[i][2] +"() {<br><br>" + tabSpace + "}<br>";
+							else
+								values += tabSpace + iterator.get().methods[i][1] + " "+ iterator.get().methods[i][2] +"() {<br>" + tabSpace + tabSpace + "return //"+ iterator.get().methods[i][1] + "<br>" + tabSpace + "}<br>";
+						}
+						else{
+							if(iterator.get().methods[i][1] == "void")
+								values += tabSpace + "void "+ iterator.get().methods[i][2] +"() = 0;<br>";
+							else
+								values += tabSpace + iterator.get().methods[i][1] + " "+ iterator.get().methods[i][2] +"() = 0;<br>";
+
+						}
+					}
+				}
+			}
+			if(iterator.get().classType == "class"){
+				values += tabSpace + "public:<br> " + tabSpace + iterator.get().className + "() {<br><br>" + tabSpace + "}<br>";
+				for(let i = 0; i < iterator.get().methods.length; i++){
+					if(iterator.get().methods[i][0] == "public"){
+						if(iterator.get().classType == "class"){
+							if(iterator.get().methods[i][1] == "void")
+								values += tabSpace + "void "+ iterator.get().methods[i][2] +"() {<br><br>" + tabSpace + "}<br>";
+							else
+								values += tabSpace + iterator.get().methods[i][1] + " "+ iterator.get().methods[i][2] +"() {<br>" + tabSpace + tabSpace + "return //"+ iterator.get().methods[i][1] + "<br>" + tabSpace + "}<br>";
+						}
+						else{
+							if(iterator.get().methods[i][1] == "void")
+								values += tabSpace + "void "+ iterator.get().methods[i][2] +"() = 0;<br>";
+							else
+								values += tabSpace + iterator.get().methods[i][1] + " "+ iterator.get().methods[i][2] +"() = 0;<br>";
+						}
+					}
+				}
+			}
 			//class scope end
-			values += "<br>}</div><br><br>"
+			values += "}</div><br>"
 		}
 		return values;
+	}
+	
+	countPrivateMethods(iterator){
+		for(let i = 0; i < iterator.get().methods.length; i++){
+			if(iterator.get().methods[i][0] == "private"){
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- js/export/PythonExporter.js
+++ js/export/PythonExporter.js
@@ -17,12 +17,20 @@ class PythonExporter extends ILanguageExporter{
 			values += ":<br>";
 			//class scope
 			if(iterator.get().classType == "class")
-				values += tabSpace + "def __init__(self): <br>" + tabSpace + tabSpace + "pass";
+				values += tabSpace + "def __init__(self): <br>" + tabSpace + tabSpace + "pass<br>";
+			if(iterator.get().methods.length != 0){
+				for(let i = 0; i < iterator.get().methods.length; i++){
+					if(iterator.get().methods[i][1] != "void")
+						values += tabSpace + "def " + iterator.get().methods[i][2] +"(): <br>" + tabSpace + tabSpace + "return <br>";
+					else
+						values += tabSpace + "def " + iterator.get().methods[i][2] +"(): <br>" + tabSpace + tabSpace + "pass <br>";
+				}
+			}
 			else
 				values += tabSpace + "pass";
 			
 			//class scope end
-			values += "<br></div><br><br>"
+			values += "</div><br>"
 		}
 		return values;
 	}
