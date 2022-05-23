3f2da08625fd53412244d2e3d9d5d46b0b8390f7
Emirhan Yener <yeneremirhann@gmail.com>
2022-05-23 20:27:39 +0300

    update test

--- tests/AddExportTest.js
+++ tests/AddExportTest.js
@@ -1,9 +1,25 @@
-//add to list
+//values
 list.add("interface", "IShape", "", "", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
 list.add("class", "Circle", "IShape", "interface", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
 list.add("class", "Rectangle", "IShape", "interface", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
 list.add("class", "Square", "Rectangle", "class", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
 
-//refresh and export
+//refresh
 refreshValues();
+
+//test
 exportStruct();
+changeClassProgrammingLanguage(new CPlusPlusExporter());
+exportStruct();
+console.log("c++ success");
+changeClassProgrammingLanguage(new CSharpExporter());
+exportStruct();
+console.log("c# success");
+changeClassProgrammingLanguage(new PythonExporter());
+exportStruct();
+console.log("python success");
+changeClassProgrammingLanguage(new JavaExporter());
+exportStruct();
+console.log("java success");
+
+alert("test success");
