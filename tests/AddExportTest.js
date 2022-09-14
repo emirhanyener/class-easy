e8367d8cec3d0178abef057e5a5841462148afdb
emirhanyener <yeneremirhann@gmail.com>
2022-09-14 22:37:05 +0300

    add test function

--- tests/AddExportTest.js
+++ tests/AddExportTest.js
@@ -1,34 +1,36 @@
-//values
-list.add("interface", "IShape", "", "", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
-list.add("class", "Circle", "IShape", "interface", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
-list.add("class", "Rectangle", "IShape", "interface", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
-list.add("class", "Square", "Rectangle", "class", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
-list.add("class", "a", "Circle", "class", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
-list.add("class", "b", "Circle", "class", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
+function test(){
+    //values
+    list.add("interface", "IShape", "", "", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
+    list.add("class", "Circle", "IShape", "interface", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
+    list.add("class", "Rectangle", "IShape", "interface", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
+    list.add("class", "Square", "Rectangle", "class", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
+    list.add("class", "a", "Circle", "class", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
+    list.add("class", "b", "Circle", "class", [["public", "float", "calculateArea"], ["private", "float", "calculatePerimeter"]]);
 
-//refresh
-refreshValues();
+    //refresh
+    refreshValues();
 
-//test
-exportStruct();
-changeClassProgrammingLanguage(new CPlusPlusExporter());
-exportStruct();
-console.log("c++ success");
-changeClassProgrammingLanguage(new CSharpExporter());
-exportStruct();
-console.log("c# success");
-changeClassProgrammingLanguage(new PythonExporter());
-exportStruct();
-console.log("python success");
-changeClassProgrammingLanguage(new PHPExporter());
-exportStruct();
-console.log("php success");
-changeClassProgrammingLanguage(new RubyExporter());
-exportStruct();
-console.log("ruby success");
-changeClassProgrammingLanguage(new JavaExporter());
-exportStruct();
-console.log("java success");
-exportDiagram();
+    //test
+    exportStruct();
+    changeClassProgrammingLanguage(new CPlusPlusExporter());
+    exportStruct();
+    console.log("c++ success");
+    changeClassProgrammingLanguage(new CSharpExporter());
+    exportStruct();
+    console.log("c# success");
+    changeClassProgrammingLanguage(new PythonExporter());
+    exportStruct();
+    console.log("python success");
+    changeClassProgrammingLanguage(new PHPExporter());
+    exportStruct();
+    console.log("php success");
+    changeClassProgrammingLanguage(new RubyExporter());
+    exportStruct();
+    console.log("ruby success");
+    changeClassProgrammingLanguage(new JavaExporter());
+    exportStruct();
+    console.log("java success");
+    exportDiagram();
 
-alert("test success");
+    alert("test success");
+}
